720464ca844d5a987f318d0d7b30b15930872714
Bradly Horn <wolfnchaos@gmail.com>
2020-07-27 16:29:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,7 +23,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return age > 12 && age < 20 ? ('You ae a teenager.') : ('You are not a tennager')
+  return age > 12 && age < 20 ? ('You are a teenager.') : ('You are not a teenager')
 }
 
 function switchAge(age) {
